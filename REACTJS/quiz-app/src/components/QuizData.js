9c138bc97ebf8c4c2d685002c59563fd7e07794c
Bijay123-pradhan <pradhanbijay29177@gmail.com>
2022-01-11 19:29:47 +0530

    added new

---
+++ REACTJS/quiz-app/src/components/QuizData.js
@@ -0,0 +1,80 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `Which type of Javscript language is _____?`,
+        options: [`Object oriented `, `Object Based`, `Assembly-Language`, `High-Level`],
+        answer: `High-Level`
+    },
+    {
+        id: 1,
+        question: 'The "Function" and "var" are known as :',
+        options: ['Keywords', 'Data Types', 'Declaration Statements', 'Prototypes'],
+        answer: 'Declaration Statements'
+    },
+    {
+        id: 2,
+        question:'In javascript , which one of the following is not considered as an error :' ,
+        options: ['Syntax error ', 'Missing of semicolons', 'Division by zero', 'Missing of bracket'],
+        answer: 'Division by zero'
+    },
+    {
+        id: 3,
+        question:'In javascript , which of the following number object function returns the value of the number ?',
+        options: ['toString() ', 'valueOf()', 'toLocaleString()', 'toPrecision'],
+        answer: 'valueOf()'
+    },
+    {
+        id: 4,
+        question:  'In javascript , which of the following is an ternary operator ?',
+        options: [' ? ',
+        ':',
+         '-',
+         '+'],
+        answer: ' ? '
+    }, 
+    {
+        id: 5,
+        question:  'In javascript , which of the following method or operator  is used for identification of the array ?',
+        options: [' Typeof ',
+        '==',
+        '===',
+        'isarrayType()'],
+        answer: 'isarrayType()'
+    },
+    {
+        id: 6,
+        question:'who is richest person in the world?'  ,
+        options: ['Jeff Bezos', 
+        'Elon Musk', 
+         'Mukesh Ambani',
+        'Warren Buffett'],
+        answer:  'Elon Musk'
+    },
+    {
+        id: 7,
+        question:'If a function which does not return a value is known as ________?',
+        options: ['Static function',
+        'Procedures',
+        'Method ', 
+         'Dynamic function'],
+        answer: 'Procedures'
+    },
+    {
+        id: 8,
+        question: 'The execution of a function stops when the program control encounters the  ________statement in the body of the function ?',
+        options: ['return statement',
+        'continue statement ',
+        'break staement',
+        'goto statement '],
+        answer: 'return statement'
+    },
+    {
+        id: 9,
+        question: 'Which of the following type of a variable is volatile ?' ,
+        options: ['Mutable variavle',
+        'Dynamic variable',
+        'Volatile variabale',
+        'Immutable variable'],
+        answer:'Mutable variavle'
+    },
+]
